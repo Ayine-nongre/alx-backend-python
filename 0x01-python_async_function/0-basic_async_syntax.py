8d66e943b9406e2ac69729b0736a72bb473f17be
Ayine-nongre <eugeneatinbire@gmail.com>
2024-03-18 13:11:03 +0000

    Adding tasks 0-2

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+This is a task to familiarize myself with async syntax in python
+"""
+
+import random
+import asyncio
+
+
+async def wait_random(max_delay: int = 10) -> float:
+    # function that returns random delay time after that period has elasped
+    delay: float = random.uniform(0, max_delay)
+    await asyncio.sleep(delay)
+    return delay
